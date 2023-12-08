1b652a4af7861c17400c4272a57e9ef52c5ca160
Shramee Srivastav <shramee.srivastav@gmail.com>
2023-12-09 00:17:54 +0530

    jsonrpc

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
-fn main() {
-    println!("Hello, world!");
+use hyper::Method;
+use jsonrpsee::server::{RpcModule, Server};
+use std::net::SocketAddr;
+use tower_http::cors::{Any, CorsLayer};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::FmtSubscriber::builder()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .try_init()
+        .expect("setting default subscriber failed");
+
+    // Start up a JSON-RPC server that allows cross origin requests.
+    let _server_addr = run_server().await?;
+
+    futures::future::pending().await
+}
+
+async fn run_server() -> anyhow::Result<SocketAddr> {
+    // Add a CORS middleware for handling HTTP requests.
+    // This middleware does affect the response, including appropriate
+    // headers to satisfy CORS. Because any origins are allowed, the
+    // "Access-Control-Allow-Origin: *" header is appended to the response.
+    let cors = CorsLayer::new()
+        // Allow `POST` when accessing the resource
+        .allow_methods([Method::POST])
+        // Allow requests from any origin
+        .allow_origin(Any)
+        .allow_headers([hyper::header::CONTENT_TYPE]);
+    let middleware = tower::ServiceBuilder::new().layer(cors);
+
+    // The RPC exposes the access control for filtering and the middleware for
+    // modifying requests / responses. These features are independent of one another
+    // and can also be used separately.
+    // In this example, we use both features.
+    let server = Server::builder()
+        .set_middleware(middleware)
+        .build("127.0.0.1:18765".parse::<SocketAddr>()?)
+        .await?;
+
+    let mut module = RpcModule::new(());
+    module.register_method("say_hello", |_, _| {
+        println!("say_hello method called!");
+        "Hello there!!"
+    })?;
+
+    let addr = server.local_addr()?;
+    let handle = server.start(module);
+
+    // In this example we don't care about doing shutdown so let's it run forever.
+    // You may use the `ServerHandle` to shut it down or manage it yourself.
+    tokio::spawn(handle.stopped());
+
+    Ok(addr)
 }
