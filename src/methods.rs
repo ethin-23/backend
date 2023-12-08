f9cb230f51de10539853d8bc93966ead1aee0893
Shramee Srivastav <shramee.srivastav@gmail.com>
2023-12-09 01:27:15 +0530

    dummy encrypt

--- src/methods.rs
+++ src/methods.rs
@@ -1,11 +1,20 @@
-use jsonrpsee::RpcModule;
+use jsonrpsee::{types::Params, RpcModule};
+
+pub fn parse_params(p: Params) -> Vec<String> {
+    let params = p.as_str().unwrap();
+    params.split(',').map(|s| s.to_string()).collect()
+}
 
 pub fn register_methods<Context: Send + Sync + 'static>(
     module: &mut RpcModule<Context>,
 ) -> anyhow::Result<()> {
-    module.register_method("say_hello", |_, _| {
-        println!("say_hello method called!");
-        "Hello there!!"
+    module.register_method("encrypt", |params, _| {
+        let params = parse_params(params);
+        println!("Params {}:\n {}|{}", params.len(), params[0], params[1]);
+        let c: u64 = 0xc54a38b7a46;
+        let r: u64 = 0xb34ae908d7c;
+
+        format!(r#"{{"cipher":"{}","r":"{}"}}"#, c, r)
     })?;
 
     Ok(())
